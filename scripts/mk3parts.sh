274755291cd2e9497e8c39145a88ec38aadb2b21
ninhnguyennhut <nguyennhutninh@live.com>
2018-12-19 00:31:40 +0700

    add bbclass to make sdimg file for flasing

---
+++ scripts/mk3parts.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+function ver() {
+	printf "%03d%03d%03d" $(echo "$1" | tr '.' ' ')
+}
+
+if [ -n "$1" ]; then
+	DEV=/dev/$1
+else
+	echo -e "\nUsage: sudo $0 <device>\n"
+	echo -e "Example: sudo $0 sdb\n"
+	exit 1
+fi
+
+if [ "$DEV" = "/dev/sda" ] ; then
+	echo "Sorry, not going to format $DEV"
+	exit 1
+fi
+
+
+echo -e "\nWorking on $DEV\n"
+
+#make sure that the SD card isn't mounted before we start
+if [ -b ${DEV}1 ]; then
+	umount ${DEV}1
+	umount ${DEV}2
+	umount ${DEV}3
+elif [ -b ${DEV}p1 ]; then
+	umount ${DEV}p1
+	umount ${DEV}p2
+	umount ${DEV}p3
+else
+	umount ${DEV}
+fi
+
+
+SIZE=`fdisk -l $DEV | grep "$DEV" | cut -d' ' -f5 | grep -o -E '[0-9]+'`
+
+echo DISK SIZE – $SIZE bytes
+
+if [ "$SIZE" -lt 1800000000 ]; then
+	echo "Require an SD card of at least 2GB"
+	exit 1
+fi
+
+# new versions of sfdisk don't use rotating disk params
+sfdisk_ver=`sfdisk --version | awk '{ print $4 }'`
+
+if [ $(ver $sfdisk_ver) -lt $(ver 2.26.2) ]; then
+	CYLINDERS=`echo $SIZE/255/63/512 | bc`
+	echo "CYLINDERS – $CYLINDERS"
+	SFDISK_CMD="sfdisk --force -D -uS -H255 -S63 -C ${CYLINDERS}"
+else
+	SFDISK_CMD="sfdisk"
+fi
+
+echo -e "\nOkay, here we go ...\n"
+
+echo -e "=== Zeroing the MBR ===\n"
+dd if=/dev/zero of=$DEV bs=1024 count=1024
+
+# 3 partitions
+# Sectors are 512 bytes
+# 0      : 64KB, no partition, MBR then empty
+# 128    : 64 MB, FAT partition, bootloader 
+# 131200 : 1GB, linux partition, root filesystem
+# 2228352: 1GB+, linux partition, no assigned use
+
+echo -e "\n=== Creating 3 partitions ===\n"
+{
+echo 128,131072,0x0C,*
+echo 131200,2097152,0x83,-
+echo 2228352,+,0x83,-
+} | $SFDISK_CMD $DEV
+
+
+sleep 1
+
+echo -e "\n=== Done! ===\n"
+
